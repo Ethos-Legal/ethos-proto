2fd89729f71a753d666264e2b7b4a63fa9bc7dc5
Garhett <garhett.l.morgan@gmail.com>
2021-04-22 18:54:16 -0700

    finished the dynamic displaying of bids for job seekers

--- src/main/java/com/ethos/legal/Controllers/JobPosterProfileController.java
+++ src/main/java/com/ethos/legal/Controllers/JobPosterProfileController.java
@@ -2,12 +2,14 @@ package com.ethos.legal.Controllers;
 
 import com.ethos.legal.Models.App_User;
 import com.ethos.legal.Models.App_User_Repository;
+import com.ethos.legal.Models.JobPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 @Controller
 public class JobPosterProfileController {
@@ -18,7 +20,7 @@ public class JobPosterProfileController {
     @GetMapping("/JobPosterProfile")
     public String JobPosterProfile(Principal principal, Model m) {
 
-
+//        ArrayList<JobPost> outstandingJobList = (ArrayList<JobPost>)jobPostRepository.findAll();
 
         App_User appUser = app_user_repository.findByEmail(principal.getName());
         m.addAttribute("principal", principal);
